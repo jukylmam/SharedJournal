13b9d75913b3516d895350367fc4d020533cd57b
Tomi Lämsä <lamsatom@gmail.com>
2017-10-27 15:50:54 +0300

    removed commented model description from journalentry.java

--- app/src/main/java/rtdsd/groupwork/sharedjournal/model/JournalEntry.java
+++ app/src/main/java/rtdsd/groupwork/sharedjournal/model/JournalEntry.java
@@ -4,41 +4,6 @@ package rtdsd.groupwork.sharedjournal.model;
  * Created by domuska on 10/27/17.
  */
 
-/*
-package models;
-public class Journal
-{
-	UUID id
-	List<UUID> entries
-	List<UUID> sessions
-	List<UUID> linkedUsers
-}
-
-public class Session
-{
-	UUID id
-	List<UUID> listEntries
-	String title
-	DateTime startedOn
-	DateTime endedOn
-}
-
-public class Entry
-{
-	UUID id
-	UUID sessionId
-	DateTime createdOn
-	DateTime modifiedOn
-	String content
-}
-
-public class User
-{
-	UUID id
-	List<UUID> linkedJournals
-}
- */
-
 public class JournalEntry {
 
     String id, sessionId, createdOn, modifiedOn, content;
